cd42aa40eae3c958274aae37800cbf4a5ac70cae
Sameer Basil <smrbasil4@gmail.com>
2024-12-02 09:02:33 +0530

    Added CORs config

--- ShortestRouteOptimizer/Program.cs
+++ ShortestRouteOptimizer/Program.cs
@@ -6,6 +6,17 @@ namespace ShortestRouteOptimizer
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            // Add CORS services
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigins", policy =>
+                {
+                    policy.WithOrigins("http://localhost:5173") // Replace with your allowed origins
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+            });
+
             // Add services to the container.
 
             builder.Services.AddControllers();
@@ -23,6 +34,7 @@ namespace ShortestRouteOptimizer
             }
 
             app.UseAuthorization();
+            app.UseCors("AllowSpecificOrigins");
 
 
             app.MapControllers();
